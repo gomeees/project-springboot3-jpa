7d1dc57af4e02db920baa52f6b6e5786b628d550
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-09 00:29:31 -0300

    Service layer, component registration

--- src/main/java/com/motosimagab/sprbootproject/repositories/UserRepository.java
+++ src/main/java/com/motosimagab/sprbootproject/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.motosimagab.sprbootproject.entities.User;
 
+// JpaRepository já tem a anotação @repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
