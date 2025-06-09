b62ed88f867ea21cea7f5be4f175ff9992bd2cc4
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-09 00:00:06 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/motosimagab/sprbootproject/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.motosimagab.sprbootproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.motosimagab.sprbootproject.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
