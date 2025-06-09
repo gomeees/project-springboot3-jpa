72fd68d0f37db8bd5850292d378b073314a1f909
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-08 22:52:40 -0300

    Project created

---
+++ src/main/java/com/motosimagab/sprbootproject/SprbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.motosimagab.sprbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprbootprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motosimagab/sprbootproject/SprbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motosimagab.sprbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
