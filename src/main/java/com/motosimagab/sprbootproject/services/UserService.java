19fe2b46d2cf1f5294e14a229ea3337d2c34b7a1
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-10 15:24:41 -0300

    Exception handling - update

--- src/main/java/com/motosimagab/sprbootproject/services/UserService.java
+++ src/main/java/com/motosimagab/sprbootproject/services/UserService.java
@@ -13,6 +13,8 @@ import com.motosimagab.sprbootproject.repositories.UserRepository;
 import com.motosimagab.sprbootproject.services.exceptions.DatabaseException;
 import com.motosimagab.sprbootproject.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -47,9 +49,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
