669337c1a1867d94c6436fa35d17e4790db54e92
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-10 14:53:11 -0300

    Exception handling - findById

--- src/main/java/com/motosimagab/sprbootproject/services/UserService.java
+++ src/main/java/com/motosimagab/sprbootproject/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.motosimagab.sprbootproject.entities.User;
 import com.motosimagab.sprbootproject.repositories.UserRepository;
+import com.motosimagab.sprbootproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
